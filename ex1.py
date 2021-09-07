d7d2f75a7674ee739cd58df9ec01fe8201863553
Your Name <malo2b>
2021-09-07 15:26:58 +0200

    update

--- ex1.py
+++ ex1.py
@@ -1,11 +1,11 @@
-def message_imc(imc:float):
+def message_imc(imc:float) -> str:
     """
     Fonction qui a partir de la valeur d'IMC passé en paramètre
     interprete le resultat
     """
     
     # CONSTANTES #
-    PALIER_IMC = [16.5,18.5,25,30,35,40]
+    PALIER_IMC = [16.5,18.5,25,30,35,40,99]
     INTERPRETATION_IMC = [
         "dénutrition ou famine",
         "maigreur",
@@ -18,7 +18,13 @@ def message_imc(imc:float):
     
     # variables #
     interpretation = ""
+    index = 0
     
-    for index, seuilImc in PALIER_IMC:
-        if imc < seuilImc:
+    while index < len(PALIER_IMC) and interpretation == "":
+        if PALIER_IMC[index] > imc:
             interpretation = INTERPRETATION_IMC[index]
+        index+=1
+        
+    return interpretation
+
+print(message_imc(17))
