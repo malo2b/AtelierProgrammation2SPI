4a07498b884ab97cda028458b8bacb648b23dc21
Your Name <malo2b>
2021-09-08 10:21:53 +0200

    Co-authored-by: FOCARDI FRANCOIS-PHILIPPE <20211336@webmail.universita.corsica>

--- ex4.py
+++ ex4.py
@@ -1,16 +1,28 @@
 from ex2 import anneeBissextile
-from datetime import date
+import datetime as date
  
 def dateEstValide(jour:int,mois:int,annee:int):
     """
     Verifie si la date entrez par l'utilisateur est valide
     """
+    
+    # CONSTANTES #
+    DAY_OF_MONTH = [
+        None,31,28,31,30,31,30,31,31,30,31,30,31 # Tableau contenant le nombre de jour dans le mois 
+    ]                                            # correspondant a l'index
+    
+    # Variables #
     estValide = False
     estBissextile = anneeBissextile(annee)
-    if(jour>=1 and jour <= 31):
-        if(mois>=1 and mois<=12):
-            if(annee>0):
+
+    if estBissextile:
+        DAY_OF_MONTH[2] = 29
+        
+    if type(annee) == int:
+        if mois >=1 and mois <= 12:
+            if jour >=1 and jour <= DAY_OF_MONTH[mois]:
                 estValide = True
+            
     return estValide
     
     
@@ -33,14 +45,36 @@ def saisieDateNaissance():
         except ValueError:
             print("Veuillez saisir une date de naissance valide !")
             
-    # return datetime.date(yyyy,mm,jj)
+    return date.date(yyyy,mm,jj)
 
+def age(dateNaissance:date):
+    """
+    Fonction retournant l'age de l'individue
+    """
     
+    dateAjourdhui = date.date.today()
+    age = dateAjourdhui - dateNaissance
+    return int(age.days/365.25)
+    
+def estMajeur(dateNaissance:date):
+    """
+    Fonction retournant True si l'individue est majeu
+    """
+    
+    AGE_MAJORITE = 18
+    DATEDAUJOURDHUI = date.date.today()
+    estMajeure = False
+    age = DATEDAUJOURDHUI.year - dateNaissance.year
+    if(age>=AGE_MAJORITE):
+        estMajeure = True
+    return estMajeure    
 
-def age(dateNaissance):
-    date.today
+def testAcces():
+    dateNaissance = saisieDateNaissance()
+    print ("Bonjour vous avez {} ans".format(age(dateNaissance)))
+    if estMajeur(dateNaissance):
+        print("Accès autorisé")
+    else:    
+        print("Accès interdit")
     
-# def estMajeure(dateNaissance):
-
-# def testAcces():
-    
+testAcces()
